97aad3ea7793ac89dab2b3b7b3a16df8ed479740
blackdwarftech <blackdwarftech@gmail.com>
2025-05-08 15:42:36 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Neuro Docs",
+    version = "0.0.0",
+    author = 'Siddhi Shrivastava',
+    author_mail = 'siddhi.atwork@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
